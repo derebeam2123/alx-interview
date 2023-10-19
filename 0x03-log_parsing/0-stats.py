64aa4f075ef8c1be116764eb26d8616cfd68370a
Nahom <derebeam2123@email.com>
2023-10-19 09:54:51 -0700

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,111 @@
 #!/usr/bin/python3
-
-
-"""
-This module holds a simple module to do log
-parsing. Given a input of log files, extract some
-usefull information and print it to standard output
-"""
-
+"""Log parser"""
 import sys
+from typing import List, Union
 import re
-def check_log_format(log_string):
-    regex_pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d+'
-    return re.match(regex_pattern, log_string)
 
 
-def print_to_stdout(**kwargs):
-    """
-    A simple helper function to print
-    the values to standard output
-    """
-    for key in kwargs.keys():
-        if not isinstance(kwargs.get(key), dict):
-            # Means this is not a dictionary of dictionaries
-            # So Its the file size
-            print("File Size {}".format(kwargs.get(key)))
+def validate_line(log: str) -> Union[List[str], None]:
+    """Check whether a line is skippable"""
+    line = log.split(" ")
+    if len(line) != 9:
+        if "-" in line[0]:
+            clean = log.replace("-", " - ", 1)
+            line = clean.split(" ")
         else:
-            for nested_k, nested_v in key.items():
-                print(f"{nested_k}: {nested_v}")
+            return None
 
+    try:
+        # Validate IP Address
+        ip_address = line[0]
+        ip_pattern = r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
+        if not re.match(ip_pattern, ip_address) and ip_address != "Holberton":
+            return None
 
-def read_and_analyze_log_files():
-    while True:
+        # Validate date
+        date = " ".join([line[2][1:], line[3][:-1]])
+        date_pattern = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}"
+        if not re.match(date_pattern, date):
+            return None
+
+        # Validate endpoint
+        endpoint = " ".join([line[4], line[5], line[6]])
+        if endpoint != '"GET /projects/260 HTTP/1.1"':
+            return None
+
+        # Validate file size
         try:
-            status_codes = {
-                "200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "403": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
+            file_size = int(line[8])
+        except (ValueError, IndexError):
+            file_size = None
+        if not file_size:
+            return None
+    except IndexError:
+        return None
 
-            }
-            total_size = 0
-            for time in range(11):
-                log_info = input("").strip()
-                if check_log_format(log_info):
-                    log_info_list = log_info.split(" ")
-                    f_size = log_info_list[-1]
-                    s_code = log_info_list[-2]
-                    status_codes[s_code] = status_codes.get(s_code) + 1
-                    total_size += int(f_size)
-                    sorted_keys_dict = {code: status_codes[code] for code in sorted(status_codes)}
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
-        except KeyboardInterrupt:
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
+    return line
+
+
+def parse_lines(lines: List[str]):
+    """Parse some lines from stdin"""
+    if not lines:
+        print("File size: 0")
+        return
+    file_size = 0
+    status_codes_count = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0,
+    }
+    for log in lines:
+        line = validate_line(log)
+        if line is None:
+            continue
+        try:
+            size = int(line[8])
+        except (ValueError, IndexError):
+            size = None
+        if size:
+            file_size += size
+        try:
+            status_code = int(line[7])
+        except (ValueError, IndexError):
+            status_code = None
+        if status_code and status_code in status_codes_count.keys():
+            status_codes_count[status_code] += 1
+    print("File size: {}".format(file_size))
+    for key, value in status_codes_count.items():
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+def extract_lines():
+    """Read a given number of lines from standard input"""
+    lines = []
+    try:
+        count = 1
+        while True:
+            line = sys.stdin.readline().rstrip()
+            if not line:
+                linesLength = len(lines)
+                if linesLength > 0 and linesLength % 10 != 0:
+                    parse_lines(lines)
+                if linesLength == 0:
+                    parse_lines(lines)
+                break
+            lines.append(line)
+            if count == 10:
+                parse_lines(lines)
+                count = 0
+            count += 1
+    except KeyboardInterrupt:
+        parse_lines(lines)
+
+
+if __name__ == "__main__":
+    extract_lines()
