65d5ab2bb6455f1f7d085875b90bc814f36680d3
Nahom <derebeam2123@email.com>
2023-10-30 08:43:14 -0700

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,111 +1,50 @@
 #!/usr/bin/python3
-"""Log parser"""
+"""
+log parsing
+"""
+
 import sys
-from typing import List, Union
 import re
 
 
-def validate_line(log: str) -> Union[List[str], None]:
-    """Check whether a line is skippable"""
-    line = log.split(" ")
-    if len(line) != 9:
-        if "-" in line[0]:
-            clean = log.replace("-", " - ", 1)
-            line = clean.split(" ")
-        else:
-            return None
-
-    try:
-        # Validate IP Address
-        ip_address = line[0]
-        ip_pattern = r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
-        if not re.match(ip_pattern, ip_address) and ip_address != "Holberton":
-            return None
-
-        # Validate date
-        date = " ".join([line[2][1:], line[3][:-1]])
-        date_pattern = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}"
-        if not re.match(date_pattern, date):
-            return None
-
-        # Validate endpoint
-        endpoint = " ".join([line[4], line[5], line[6]])
-        if endpoint != '"GET /projects/260 HTTP/1.1"':
-            return None
-
-        # Validate file size
-        try:
-            file_size = int(line[8])
-        except (ValueError, IndexError):
-            file_size = None
-        if not file_size:
-            return None
-    except IndexError:
-        return None
-
-    return line
-
-
-def parse_lines(lines: List[str]):
-    """Parse some lines from stdin"""
-    if not lines:
-        print("File size: 0")
-        return
-    file_size = 0
-    status_codes_count = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0,
-    }
-    for log in lines:
-        line = validate_line(log)
-        if line is None:
-            continue
-        try:
-            size = int(line[8])
-        except (ValueError, IndexError):
-            size = None
-        if size:
-            file_size += size
-        try:
-            status_code = int(line[7])
-        except (ValueError, IndexError):
-            status_code = None
-        if status_code and status_code in status_codes_count.keys():
-            status_codes_count[status_code] += 1
-    print("File size: {}".format(file_size))
-    for key, value in status_codes_count.items():
-        if value != 0:
-            print("{}: {}".format(key, value))
-
-
-def extract_lines():
-    """Read a given number of lines from standard input"""
-    lines = []
-    try:
-        count = 1
-        while True:
-            line = sys.stdin.readline().rstrip()
-            if not line:
-                linesLength = len(lines)
-                if linesLength > 0 and linesLength % 10 != 0:
-                    parse_lines(lines)
-                if linesLength == 0:
-                    parse_lines(lines)
-                break
-            lines.append(line)
-            if count == 10:
-                parse_lines(lines)
-                count = 0
-            count += 1
-    except KeyboardInterrupt:
-        parse_lines(lines)
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
 if __name__ == "__main__":
-    extract_lines()
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
