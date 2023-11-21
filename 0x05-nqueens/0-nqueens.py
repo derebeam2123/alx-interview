f8c1d340aa46c1551a93e13e838c6bf29e5e4ecb
Nahom <derebeam2123@email.com>
2023-11-21 02:36:10 -0800

    task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,97 +1,116 @@
 #!/usr/bin/python3
-"""0-nqueens module that solves the N queens problem
-"""
+
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    dimension = int(sys.argv[1])
+except Exception as E:
+    print("N must be a number")
+    exit(1)
 
-def nqueens(num):
-    """nqueens function that solves the N queens problem
+if dimension < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    Args:
-        num (int): number of queens
-    """
-    board = [[0 for x in range(num)] for y in range(num)]
-    solve_nqueens(board, 0, num)
-
-
-def solve_nqueens(board, col, num):
-    """solve_nqueens function that solves the N queens problem
-
-    Args:
-        board (list): board list
-        col (int): column number
-        num (int): number of queens in board
-
-    Returns:
-        bool: True if safe, False otherwise
-    """
-    if col == num:
-        print_board(board)
-        return True
-
-    res = False
-    for i in range(num):
-        if is_safe(board, i, col, num):
-            board[i][col] = 1
-            res = solve_nqueens(board, col + 1, num) or res
-            board[i][col] = 0
-    return res
+board = {}
+for row in range(0, dimension):
+    for column in range(0, dimension):
+        placement = (row, column)
+        board[placement] = "#"
 
 
-def is_safe(board, row, col, num):
-    """is_safe function that checks if a queen can be placed on board
-
-    Args:
-        board (list): board list
-        row (int): row number
-        col (int): column number
-        num (int): number of queens in board
-
-    Returns:
-        bool: True if safe, False otherwise
-    """
-    safe_row = row
-    safe_col = col
-    while safe_col >= 0 and safe_row < num:
-        if board[safe_row][safe_col] == 1:
-            return False
-        safe_row += 1
-        safe_col -= 1
-
-    safe_row = row
-    safe_col = col
-    while safe_row >= 0 and safe_col >= 0:
-        if board[safe_row][safe_col] == 1:
-            return False
-        safe_row -= 1
-        safe_col -= 1
-
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+def validate(board, spots):
+    """ Valitate the board """
+    if len([i for i in board.values() if i == "#"]) < spots and spots != 0:
+        return False
     return True
 
 
-def print_board(board):
-    """print_board function that prints the board
+def solve(dimension, index, board, safe_T, spots_L):
+    """ main loop for recursive """
+    if len(safe_T) > dimension:
+        return False
+    moved_board = fill_block(dimension, index, board)
+    if validate(moved_board, spots_L) and moved_board:
+        spots_L -= 1
+        inital = (index[0] + 1, index[1])
+        for i in range(dimension):
+            try:
+                if moved_board[(inital[0], i)] == "#":
+                    safe_T.append([inital[0], i])
+                    if not solve(
+                            dimension, (inital[0], i), moved_board,
+                            safe_T, spots_L):
+                        safe_T.pop()
+                        continue
+            except BaseException:
+                continue
+    if spots_L < 0 and len(safe_T) == dimension:
+        print(safe_T)
+    else:
+        return False
 
-    Args:
-        board (list): board list
-    """
-    print([[i, j] for i in range(
-        len(board)) for j in range(len(board)) if board[i][j] == 1])
+
+def fill_block(dimension, index, board_T):
+    """ function to fill queens available spots """
+    board = board_T.copy()
+    board[index] = 0
+    diagonal_C_neg_x = []
+    diagonal_C_pos_x = []
+    diagonal_C_neg_y = []
+    diagonal_C_pos_y = []
+    x = index[1]
+    while x - 1 >= 0:
+        less_x = (index[0], x - 1)
+        diagonal_C_neg_x.append(x - 1)
+        if board.get(less_x) == 0:
+            return False
+        board[less_x] = 1
+        x -= 1
+
+    y = index[0]
+    while y - 1 >= 0:
+        less_y = (y - 1, index[1])
+        diagonal_C_neg_y.append(y - 1)
+        if board.get(less_y) == 0:
+            return False
+        board[less_y] = 1
+        y -= 1
+
+    x = index[1]
+    while x + 1 < dimension:
+        more_x = (index[0], x + 1)
+        diagonal_C_pos_x.append(x + 1)
+        if board.get(more_x) == 0:
+            return False
+        board[more_x] = 1
+        x += 1
+
+    y = index[0]
+    while y + 1 < dimension:
+        more_y = (y + 1, index[1])
+        diagonal_C_pos_y.append(y + 1)
+        if board.get(more_y) == 0:
+            return False
+        board[more_y] = 1
+        y += 1
+
+    upper_left = tuple(zip(diagonal_C_pos_y, diagonal_C_neg_x))
+    upper_right = tuple(zip(diagonal_C_pos_y, diagonal_C_pos_x))
+    lower_left = tuple(zip(diagonal_C_neg_y, diagonal_C_neg_x))
+    lower_right = tuple(zip(diagonal_C_neg_y, diagonal_C_pos_x))
+    for ind in (upper_left + upper_right + lower_left + lower_right):
+        if board.get(ind) == 0:
+            return False
+        board[ind] = 1
+    return board
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        NUM = int(sys.argv[1])
-        if NUM < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        nqueens(NUM)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+for i in range(0, dimension):
+    inital = (0, i)
+    safe_T = [list(inital)]
+    spots_L = dimension - 1
+    solve(dimension, inital, board, safe_T, spots_L)
